976156a36170ceb06763f0922ea2c35011b087da
Neel Joshi <54106959+neeldjoshi97@users.noreply.github.com>
2019-12-11 22:39:55 +0530

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -3,51 +3,63 @@
 
 # reading in all the files one by one
 
-t1<-read.table("body_acc_x_test.txt")
-t2<-read.table("body_acc_y_test.txt")
-t3<-read.table("body_acc_z_test.txt")
-t4<-read.table("body_gyro_x_test.txt")
-t4<-read.table("body_gyro_y_test.txt")
-t5<-read.table("body_gyro_z_test.txt")
-t6<-read.table("total_acc_x_test.txt")
-t7<-read.table("total_acc_y_test.txt")
-t8<-read.table("total_acc_z_test.txt")
+t1<-read.table("X_test.txt")
+t2<-read.table("Y_test.txt")
+c1<-read.table("subject_test.txt")
+table1<-cbind(c1,t2,t1)
 
 # changing the working directory to the other destination if required
 # and reading the rest of the files
 
-t9<-read.table("body_acc_x_train.txt")
-t10<-read.table("body_acc_y_train.txt")
-t11<-read.table("body_acc_z_train.txt")
-t12<-read.table("body_gyro_x_train.txt")
-t13<-read.table("body_gyro_y_train.txt")
-t14<-read.table("body_gyro_z_train.txt")
-t15<-read.table("total_acc_x_train.txt")
-t16<-read.table("total_acc_y_train.txt")
-t17<-read.table("total_acc_z_train.txt")
+t3<-read.table("X_train.txt")
+t4<-read.table("Y_train.txt")
+c2<-read.table("subject_train.txt")
+table2<-cbind(c2,t4,t3)
 
 
 # Combining all the tables into one
 
 
-big_data<-rbind(t1,t2,t3,t4,t5,t6,t7,t8,t9,t10,t11,t12,t13,t14,t15,t16,t17)
+big_data<-rbind(table1,table2)
 
 
 # taking out required columns only
 
-req_data<-big_data[,1:6]
+names<-read.table("features.txt")
+col_names<-big_data[,c(1,2,grep("mean|std", names$V2)+2)]
 
-# almost done
-
-# Changing the Column names
-
-colnames(req_data)<-c("WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
-
-# successfully changed
-
-# finding the tidy results
+# preparing for the activity names
 
 library(dplyr)
-tidy<-summarise_each(req_data,mean)
+labels<-read.table("activity_labels.txt")
+label<-mutate(col_names, V1.1=labels[V1.1,2])
 
-# end of program
+# combining the data set with labels
+
+colnames(label)<-(sub("[Vv]","",colnames(label)))
+
+colnames(label)[1:3]<-c("Subjects","Activities",1)
+
+colnames(label)[3:81]<-as.character(names[as.integer(colnames(label)[3:81]),2])
+
+# combining the variable names
+
+names(label)<-gsub("^t","Time",names(label))
+names(label)<-gsub("^f","Frequency",names(label))
+names(label)<-gsub("Acc","Accelerometer",names(label))
+names(label)<-gsub("Gyro","Gyroscope",names(label))
+names(label)<-gsub("Mag","Magnitude",names(label))
+names(label)<-gsub("[()]","",names(label))
+
+# summarising the tidied data set
+
+data1<-group_by(label,Subjects,Activities)
+data1<- summarise_each(data1,mean)
+
+# saving a copy of the final version
+
+write.table( data1,file = "tidy_data.txt" ,row.names = FALSE)
+
+### End of Program
+
+
